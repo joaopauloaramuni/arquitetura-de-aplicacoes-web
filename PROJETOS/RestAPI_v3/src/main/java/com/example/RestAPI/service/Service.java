f6a5c53837d491abcb04d6a531dc51aaa5e81fe7
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-01-17 18:02:13 -0300

    Add files via upload

---
+++ PROJETOS/RestAPI_v3/src/main/java/com/example/RestAPI/service/Service.java
@@ -0,0 +1,10 @@
+package com.example.RestAPI.service;
+
+public class Service {
+    public String welcomeMessage() {
+        return "Essa é minha primeira API REST";
+    }
+    public String exampleMessage() {
+        return "Mensagem de exemplo";
+    }
+}
