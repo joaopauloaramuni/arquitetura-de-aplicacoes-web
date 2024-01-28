010e793877c6a5b110816f15ade3b1e4e70d189c
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-01-28 09:33:31 -0800

    Add files via upload

---
+++ PROJETOS/View_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
@@ -0,0 +1,7 @@
+package com.example.RestAPI.service;
+
+public class Service {
+    public String exampleView() {
+        return "Acesse <a href='http://localhost:8080/'>localhost:8080</a> para visualizar o contéudo do arquivo resources/static/index.html.";
+    }
+}
