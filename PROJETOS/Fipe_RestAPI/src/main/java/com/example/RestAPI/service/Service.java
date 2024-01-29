b29b75bcaf7225fae3d4e61c7785dc6893af6b16
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-01-29 17:05:59 -0300

    comitao

---
+++ PROJETOS/Fipe_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
@@ -0,0 +1,30 @@
+package com.example.RestAPI.service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class Service {
+
+    private String consultarURL(String apiUrl){
+        String dados = "";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiUrl, String.class);
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            dados = responseEntity.getBody();
+        } else {
+            dados = "Falha ao obter dados. Código de status: " + responseEntity.getStatusCode();
+        }
+        return dados;
+    }
+    public String consultarMarcas() {
+        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+    }
+    public String consultarModelos(int id) {
+        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+id+"/modelos");
+    }
+    public String consultarAnos(int marca, int modelo) {
+        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos");
+    }
+    public String consultarValor(int marca, int modelo, String ano) {
+        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos/"+ano);
+    }
+}
---
+++ PROJETOS/JavaDoc_JWT_RestAPI/src/main/java/com/example/JWT_RestAPI/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.JWT_RestAPI.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(HttpMethod.POST, "/login/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/username/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated()
+                ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("joao")
+                .password(passwordEncoder().encode("4321"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
