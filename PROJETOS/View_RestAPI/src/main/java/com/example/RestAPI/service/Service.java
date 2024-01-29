95cd78cfc1863dda211fe3291a650be050391f6d
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-01-29 02:39:31 -0300

    Delete PROJETOS/View_RestAPI directory

--- PROJETOS/View_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
+++
@@ -1,7 +0,0 @@
-package com.example.RestAPI.service;
-
-public class Service {
-    public String exampleView() {
-        return "Acesse <a href='http://localhost:8080/'>localhost:8080</a> para visualizar o contéudo do arquivo resources/static/index.html.";
-    }
-}
