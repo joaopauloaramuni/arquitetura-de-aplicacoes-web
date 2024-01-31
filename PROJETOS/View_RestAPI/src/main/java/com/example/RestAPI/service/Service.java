c8b6b26e3ff14de3fa99de795b5df3a7bc5901dc
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-01-31 09:57:25 -0300

    Delete PROJETOS directory

--- PROJETOS/View_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
+++
@@ -1,8 +0,0 @@
-package com.example.RestAPI.service;
-
-@org.springframework.stereotype.Service
-public class Service {
-    public String exampleView() {
-        return "Acesse <a href='http://localhost:8080/'>localhost:8080</a> para visualizar o contéudo do arquivo resources/static/index.html.";
-    }
-}
