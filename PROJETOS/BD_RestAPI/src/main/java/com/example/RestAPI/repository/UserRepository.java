b72d02f17b184c75c94b67eb8b36c25c0d5578a9
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-03-22 19:43:54 -0300

    Update UserRepository.java

--- PROJETOS/BD_RestAPI/src/main/java/com/example/RestAPI/repository/UserRepository.java
+++ PROJETOS/BD_RestAPI/src/main/java/com/example/RestAPI/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.RestAPI.repository;
 import com.example.RestAPI.model.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.mongodb.repository.Query;
 import java.util.List;
 
 @Repository
