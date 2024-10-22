6d9caaabf25272555b2662623e516dd21b81622f
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-10-22 13:43:31 -0300

    Add files via upload

---
+++ PROJETOS/Url_Shortener_v1/src/test/java/com/example/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={com.example.urlshortener.application.UrlshortenerApplication.class})
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
