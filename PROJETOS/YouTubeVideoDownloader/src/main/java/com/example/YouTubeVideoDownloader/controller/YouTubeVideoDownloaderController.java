68a387c4695a9ac4669ed07342a6f2c05b05fe26
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-10-27 00:18:17 -0300

    Add files via upload

---
+++ PROJETOS/YouTubeVideoDownloader/src/main/java/com/example/YouTubeVideoDownloader/controller/YouTubeVideoDownloaderController.java
@@ -0,0 +1,20 @@
+package com.example.YouTubeVideoDownloader.controller;
+
+import com.example.YouTubeVideoDownloader.dto.VideoRequestDTO;
+import com.example.YouTubeVideoDownloader.service.YouTubeVideoDownloaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/videos")
+public class YouTubeVideoDownloaderController {
+
+    @Autowired
+    private YouTubeVideoDownloaderService youTubeVideoDownloaderService;
+
+    @PostMapping("/download")
+    public String downloadVideo(@RequestBody VideoRequestDTO videoRequestDTO) {
+        youTubeVideoDownloaderService.downloadVideo(videoRequestDTO.getUrl(), videoRequestDTO.getPath());
+        return "Download iniciado!";
+    }
+}
