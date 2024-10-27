2ac5c5ddc6dd12ec2a0b70f48a240ddebe81a1fb
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2024-10-27 19:23:00 -0300

    Add files via upload

---
+++ PROJETOS/TwilioWhatsAppMessenger/src/test/java/com/example/TwilioWhatsAppMessenger/TwilioWhatsAppMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TwilioWhatsAppMessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={com.example.TwilioWhatsAppMessenger.application.TwilioWhatsAppMessengerApplication.class})
+class TwilioWhatsAppMessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
