3bd24e0c6205315fe5a46ab31c15fb62087b5130
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2025-09-01 19:12:34 -0300

    Import List class in HuggingfaceApiService

--- PROJETOS/HuggingfaceAPI/src/main/java/com/example/HuggingfaceAPI/service/HuggingfaceApiService.java
+++ PROJETOS/HuggingfaceAPI/src/main/java/com/example/HuggingfaceAPI/service/HuggingfaceApiService.java
@@ -7,6 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class HuggingfaceApiService {
