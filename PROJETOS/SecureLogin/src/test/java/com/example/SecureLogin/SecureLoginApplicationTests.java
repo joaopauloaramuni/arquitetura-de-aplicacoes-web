35f6a8990cb1ccee2aca852ea53d3ef6bd85e983
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2025-09-08 18:54:33 -0300

    Add files via upload

---
+++ PROJETOS/SecureLogin/src/test/java/com/example/SecureLogin/SecureLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecureLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes={com.example.SecureLogin.application.SecureLoginApplication.class})
+class SecureLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
