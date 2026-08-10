a96d109c33333e68f0170b127ecefeae30b5aaac
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-10 17:51:09 -0300

    Specify application class in Spring Boot test

--- PROJETOS/Fipe_RestAPI/src/test/java/com/example/RestAPI/RestApiApplicationTests.java
+++ PROJETOS/Fipe_RestAPI/src/test/java/com/example/RestAPI/RestApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.RestAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={com.example.RestAPI.application.RestApiApplication.class})
 class RestApiApplicationTests {
 
 	@Test
