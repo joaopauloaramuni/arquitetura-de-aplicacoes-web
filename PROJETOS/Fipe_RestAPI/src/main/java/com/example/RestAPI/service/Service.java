fa0db7cba09c2a84ab7775f601318d98f059fade
João Paulo Aramuni <joaopauloaramuni@gmail.com>
2026-08-18 18:13:00 -0300

    Refactor API URLs to use BASE_URL constant

--- PROJETOS/Fipe_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
+++ PROJETOS/Fipe_RestAPI/src/main/java/com/example/RestAPI/service/Service.java
@@ -4,6 +4,9 @@ import org.springframework.web.client.RestTemplate;
 
 public class Service {
 
+    private static final String BASE_URL = "https://fipe.parallelum.com.br/api/v2";
+
+    // https://fipe.api.br/docs/api/fipe
     private String consultarURL(String apiUrl){
         String dados = "";
         RestTemplate restTemplate = new RestTemplate();
@@ -16,15 +19,19 @@ public class Service {
         return dados;
     }
     public String consultarMarcas() {
-        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+        // return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas");
+        return consultarURL(BASE_URL + "/cars/brands");
     }
     public String consultarModelos(int id) {
-        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+id+"/modelos");
+        // return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+id+"/modelos");
+        return consultarURL(BASE_URL + "/cars/brands/"+id+"/models");
     }
     public String consultarAnos(int marca, int modelo) {
-        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos");
+        // return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos");
+        return consultarURL(BASE_URL + "/cars/brands/"+marca+"/models/"+modelo+"/years");
     }
     public String consultarValor(int marca, int modelo, String ano) {
-        return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos/"+ano);
+        // return consultarURL("https://parallelum.com.br/fipe/api/v1/carros/marcas/"+marca+"/modelos/"+modelo+"/anos/"+ano);
+        return consultarURL(BASE_URL + "/cars/brands/"+marca+"/models/"+modelo+"/years/"+ano);
     }
 }
